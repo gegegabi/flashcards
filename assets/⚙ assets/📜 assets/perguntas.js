ef0c8f5b2e2990fafb93bedb1d0c7466c4627566
gegegabi <geovanna.riggo@escola.pr.gov.br>
2025-08-20 12:22:22 -0300

    Create perguntas.js

---
+++ assets/⚙ assets/📜 assets/perguntas.js
@@ -0,0 +1,11 @@
+// Lista de perguntas e respostas sobre Tinker Bell
+criaCartao("Tinker Bell", "Qual é a principal habilidade da Tinker Bell?", "Ela é uma fada talentosa que cria invenções mágicas.");
+criaCartao("Amizade", "Quem é a irmã da Tinker Bell?", "Periwinkle, fada do gelo.");
+criaCartao("Personagens", "Qual fada cuida dos animais?", "Fawn, a fada dos animais.");
+criaCartao("Natureza", "Quem é a fada responsável pela água?", "Silvermist, a fada da água.");
+criaCartao("Flores", "Qual fada adora jardins e flores?", "Rosetta.");
+criaCartao("Luz", "Qual fada controla a luz?", "Iridessa.");
+criaCartao("Vento", "Quem é a fada mais veloz?", "Vidia.");
+criaCartao("Alquimia", "Qual fada adora fazer experiências com pó mágico?", "Zarina.");
+criaCartao("Dupla", "Quem são os amigos inventores da Tinker Bell?", "Clank e Bobble.");
+criaCartao("Realeza", "Quem governa o Vale das Fadas?", "Rainha Clarion.");
